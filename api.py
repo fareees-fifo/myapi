56cbd1fdb721d039f71506ae2ff788f88712f896
fareees-fifo <miordra@gmail.com>
2026-02-28 05:27:49 +0100

    Update api.py

--- api.py
+++ api.py
@@ -1,14 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Bot is running successfully!"
-    import os
 
-if __name__ == "__main__":
+if name == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
-
