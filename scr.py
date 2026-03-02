46022713a5f14748894ff86be7d711195e771972
fareees-fifo <miordra@gmail.com>
2026-03-02 18:26:07 +0100

    Update scr.py

--- scr.py
+++ scr.py
@@ -14,9 +14,9 @@ from database import get_or_create_user, update_user_credits, get_user_credits
 from plans import get_user_current_tier
 
 # Hardcoded API credentials (as requested)
-API_ID = 
-API_HASH = "08eb23e7f0599533829fbd4b6f2d8eb5"
-BOT_TOKEN = "8010787349:AAHlUXqkjRVs4Kyuq13DxZnA9qBR7rPW9fs"
+API_ID = 1710051635
+API_HASH = "939745cea0d5ef543278c0d96231dd00"
+BOT_TOKEN = "6899797841:AAHsicM8rF-kIiGfKP87xKC6k6TswwlGzH0"
 SESSION_STRING = "BAIfSAoASrjYWs8IOVa_x4O7vCg_hP5W1aPmRx_4DT_zMWMbz4YqTkcRMnuU-EUCfCfi2Hk-9xvfna2h_trnS-EqTXjvjK4i0JPBRxna_-NWgCOe32fpAKMfVqbuOPc5MMfutXARNAeED2heBoVldVKI2JijqbXS8hOoQzY5XBjLxfTKgkJNDBVZDG4FGx_XU0ozKBxbYsQHe7NTNpHiOl6kcSKY2goxNmAHJ0_ChBEH-qeMwNUlsVo2hh2lV63miDwrUIKBANO0UM0WCm6oEkIG6aah-0kMuKnPB9AlB11Y9XH2Xg0xF8Q7aF023hS2yAEATmlsi7wMitCam2lum91jzg0VfwAAAABl7VEzAA"
 # Hardcoded limits and settings (as requested)
 DEFAULT_LIMIT = 2000  # Card Scrapping Limit For Free/Trial Users
@@ -683,4 +683,5 @@ __all__ = [
     'initialize_pyrogram',
     'stop_pyrogram'
 ]
+
 
