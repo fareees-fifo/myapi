3ec193074d9b42aa58ecf853f72cd416c89446d7
fareees-fifo <miordra@gmail.com>
2026-03-13 13:58:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 # Pyrogram setup
 API_ID =   # Replace this API ID with your actual API ID
 API_HASH = ""  # Replace this API HASH with your actual API HASH
-SESSION_STRING = "BAIfSAoASrjYWs8IOVa_x4O7vCg_hP5W1aPmRx_4DT_zMWMbz4YqTkcRMnuU-EUCfCfi2Hk-9xvfna2h_trnS-EqTXjvjK4i0JPBRxna_-NWgCOe32fpAKMfVqbuOPc5MMfutXARNAeED2heBoVldVKI2JijqbXS8hOoQzY5XBjLxfTKgkJNDBVZDG4FGx_XU0ozKBxbYsQHe7NTNpHiOl6kcSKY2goxNmAHJ0_ChBEH-qeMwNUlsVo2hh2lV63miDwrUIKBANO0UM0WCm6oEkIG6aah-0kMuKnPB9AlB11Y9XH2Xg0xF8Q7aF023hS2yAEATmlsi7wMitCam2lum91jzg0VfwAAAABl7VEzAA"
-DEFAULT_LIMIT = 1000  # Card Scrapping Limit For Everyone
+SESSION_STRING = 
 PREMIUM_LIMIT = 5000  # Card Scrapping Limit For Premium Users
 
 # Cooldown settings (in seconds)
@@ -11,4 +10,5 @@ COOLDOWN_TRIAL = 10  # Cooldown for Trial users
 
 # Credit cost per command
 CREDIT_COST = 2  # Credits deducted for each successful command
+
 
